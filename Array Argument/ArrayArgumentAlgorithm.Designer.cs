d34e8a9ce9087b80c97a7cea948a55499b1bc524
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 15:52:18 -0500

    Completed project

---
+++ Array Argument/ArrayArgumentAlgorithm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Array_Argument
+{
+    partial class ArrayArgumentAlgorithmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.findMaxBtn = new System.Windows.Forms.Button();
+            this.findMinBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.ItemHeight = 16;
+            this.outputListBox.Location = new System.Drawing.Point(57, 15);
+            this.outputListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(249, 260);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(129, 283);
+            this.goButton.Margin = new System.Windows.Forms.Padding(4);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(100, 28);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "&Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(188, 411);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 28);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // findMaxBtn
+            // 
+            this.findMaxBtn.Location = new System.Drawing.Point(80, 347);
+            this.findMaxBtn.Name = "findMaxBtn";
+            this.findMaxBtn.Size = new System.Drawing.Size(100, 28);
+            this.findMaxBtn.TabIndex = 3;
+            this.findMaxBtn.Text = "&Find Max";
+            this.findMaxBtn.UseVisualStyleBackColor = true;
+            this.findMaxBtn.Click += new System.EventHandler(this.FindMaxBtn_Click);
+            // 
+            // findMinBtn
+            // 
+            this.findMinBtn.Location = new System.Drawing.Point(186, 347);
+            this.findMinBtn.Name = "findMinBtn";
+            this.findMinBtn.Size = new System.Drawing.Size(102, 28);
+            this.findMinBtn.TabIndex = 4;
+            this.findMinBtn.Text = "Find &Min";
+            this.findMinBtn.UseVisualStyleBackColor = true;
+            this.findMinBtn.Click += new System.EventHandler(this.FindMinBtn_Click);
+            // 
+            // ArrayArgumentAlgorithmForm
+            // 
+            this.AcceptButton = this.goButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(367, 465);
+            this.Controls.Add(this.findMinBtn);
+            this.Controls.Add(this.findMaxBtn);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.outputListBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ArrayArgumentAlgorithmForm";
+            this.Text = "Array Argument Algorithm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button findMaxBtn;
+        private System.Windows.Forms.Button findMinBtn;
+    }
+}
+
---
+++ Array Argument/ArrayArgumentAlgorithm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : Array Arguments Algorithm
+* Author      : Kabrina Brady
+* Created     : 10/8/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : When Go is clicked, random array is generated. Find Max button finds largest number. Find Min button finds smallest number.
+*               Input:  Button clicks
+*               Output: Numbers in array, MessageBoxes
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+namespace Array_Argument
+{
+    public partial class ArrayArgumentAlgorithmForm : Form
+    {
+        //creates global array and random object
+        int[] randomArray = new int[15]; //creates new array with 15 values
+        Random randInt = new Random(); //creates new random object
+
+        public ArrayArgumentAlgorithmForm()
+        {
+            InitializeComponent();
+        }
+
+        //FindMax method
+        private void FindMax(int[]iArray)
+        {
+            int highest = iArray[0]; //makes the highest value the value of the 0th index in the array
+
+            for (int index = 1; index < iArray.Length; index++) //starts with the value of the 1st index in the array, continues executing as long as the end of the array hasn't been reached, and adds one to index each execution
+            {
+                if (iArray[index] > highest)
+                {
+                    highest = iArray[index]; //set the new highest value if current value is greater than current max
+                }
+            }
+
+            MessageBox.Show("The highest value is " + highest); //display message
+        }
+
+        //FindMin method
+        private void FindMin(int[] iArray)
+        {
+            int lowest = iArray[0]; //makes the lowest value the value of the 0th index in the array
+
+            for (int index = 1; index < iArray.Length; index++) //starts with the value of the 1st index in the array, continues executing as long as the end of the array hasn't been reached, and adds one to index each execution
+            {
+                if (iArray[index] < lowest)
+                {
+                    lowest = iArray[index]; //set the new lowest value if current value is greater than current min
+                }
+            }
+
+            MessageBox.Show("The lowest value is " + lowest); //display message
+        }
+
+        // Click event handler for the goButton control.
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            for (int index = 0; index < randomArray.Length; index++) //as long as index is less than 15, increment index
+            {
+                randomArray[index] = randInt.Next(200); //keep generating random numbers and putting them into randomArray
+                outputListBox.Items.Add(randomArray[index]); //display contents of the array in listbox
+            }
+        }
+
+        // Click event handler for the exitButton control.
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        //Find Max button click
+        private void FindMaxBtn_Click(object sender, EventArgs e)
+        {
+            //call FindMax method
+            FindMax(randomArray);
+        }
+
+        //Find Min button click
+        private void FindMinBtn_Click(object sender, EventArgs e)
+        {
+            //call FindMin method
+            FindMin(randomArray);
+        }
+    }
+}
